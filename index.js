75dcb8c89751d84389a571c6274bf6092d7b3b17
SUMIT PRAJAPATI <155364257+sumitprajapati1@users.noreply.github.com>
2024-09-11 18:24:20 +0530

    Finall commit

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import { OpenAI } from 'openai';
+import OpenAI from 'openai';
+import readline from 'readline';
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey:process.env.OPENAI_API_KEY
 });
-console.log(process.env.OPENAI_API_KEY); 
-async function main() {
-    try {
-        const chatCompletion = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages: [
-                { role: "user", content: "What is the capital of India?" }
-            ],
-        });
-        console.log(chatCompletion.choices[0].message.content);
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
 
-main();
+const userInterface=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+userInterface.setPrompt("You: ")
+userInterface.prompt();
+
+userInterface.on("line",async (input)=>{
+    await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content:input }]
+      }).then((result) => {
+        console.log("AI: ",result.data.choices[0].message.content);
+        userInterface.prompt();
+      }).catch((error) => {
+        console.error(error);
+      });
+      
+})
+
